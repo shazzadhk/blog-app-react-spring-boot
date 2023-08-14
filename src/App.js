b66711a7d4962772e978e70eb80f4056d616349f
Shazzad Hossain <shazzad.khan@technonext.com>
2023-08-14 15:31:50 +0600

    change something

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import {Container,Row,Col} from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        <Container>
+            <Row>
+                <Col style={{height:'100px'}} sm={12}>sm=12</Col>
+                {/*<Col sm={4}>sm=4</Col>*/}
+            </Row>
+            <Row>
+                <Col sm={8}>sm=8</Col>
+                <Col sm={4}>sm=4</Col>
+            </Row>
+        </Container>
+    </>
   );
 }
 
