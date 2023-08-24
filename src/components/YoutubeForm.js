bf574a24066ce969186faaf89a1a3356f90fe8f6
Shazzad Hossain <shazzad.khan@technonext.com>
2023-08-24 17:28:37 +0600

    use material ui in create and view page

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -1,27 +1,28 @@
-import {useForm} from "react-hook-form";
-import {DevTool} from "@hookform/devtools";
+import {useFieldArray, useForm} from "react-hook-form";
+// import {DevTool} from "@hookform/devtools";
 import {useEffect, useState} from "react";
 import '../App.css'
 import axios from "axios";
+import {Button, MenuItem, Stack, TextField} from "@mui/material";
 
 const YoutubeForm = () => {
 
-    // const [formValue, setFormValue] = useState({})
     const [addressList,setAddressList] = useState([])
     const form = useForm()
     const {register, handleSubmit, control} = form
     const baseEmpUrl = "http://localhost:8790/api/employee"
     const baseAddressUrl = "http://localhost:8790/api/address/get-all"
-    const [addressId,setAddressId] = useState("")
     const [errorValue,setErrorValue] = useState(null)
+    // const {fields,append,remove} = useFieldArray({name: "phNumber",control})
 
     useEffect(() => {
         axios.get(baseAddressUrl).then(response => {
             setAddressList(response.data)
         })
     }, []);
+
     const handleOnSubmit = (data) => {
-        data = {...data,addressIds: [parseInt(addressId)]}
+        data.addressIds = [parseInt(data.addressIds)]
         axios.post(
             baseEmpUrl, data
         ).then(response => console.log(response))
@@ -30,46 +31,97 @@ const YoutubeForm = () => {
     }
 
     return (
-        <div>
-            <h3>Add New Employee</h3>
+        <>
+            {/*<h3>Add New Employee</h3>*/}
+            {/*<form onSubmit={handleSubmit(handleOnSubmit)} noValidate>*/}
+            {/*    <label htmlFor="username">Name: </label>*/}
+            {/*    <input type="text" id="name" {...register("name")}/>*/}
+            {/*    /!*{errors.name?.message && <p className="error">{errors.name?.message}</p>}*!/*/}
+            {/*    {errorValue?.name && <p className="error">{errorValue.name}</p>}*/}
+            {/*    <br/>*/}
+
+            {/*    <label htmlFor="department">Department: </label>*/}
+            {/*    <input type="text" id="department" {...register("department")}/>*/}
+            {/*    {errorValue?.department && <p className="error">{errorValue.department}</p>}*/}
+            {/*    <br/>*/}
+            {/*    <label htmlFor="companyName">Company Name: </label>*/}
+            {/*    <input type="text" id="companyName" {...register("companyName")}/>*/}
+            {/*    {errorValue?.companyName && <p className="error">{errorValue.companyName}</p>}*/}
+            {/*    <br/>*/}
+
+            {/*    <label htmlFor="addressIds">Address: </label>*/}
+            {/*    <select id="addressIds" {...register("addressIds")} multiple>*/}
+            {/*        <option value="">::Select One::</option>*/}
+            {/*        {*/}
+            {/*            addressList.map((address) => {*/}
+            {/*                return(*/}
+            {/*                    <option key={address.id} value={address.id}>{address.city}</option>*/}
+            {/*                    )*/}
+            {/*            })*/}
+            {/*        }*/}
+            {/*    </select>*/}
+            {/*    <br/>*/}
+
+            {/*    <div>*/}
+            {/*        <label>List of Addresses</label><br/>*/}
+            {/*        {*/}
+            {/*            fields.map((field,index) => {*/}
+            {/*                return(*/}
+            {/*                    <div className="form-control" key={field.id}>*/}
+            {/*                        <input type="text" {...register(`phNumber.${index}.number`)}/>*/}
+            {/*                        {index > 0 && <button onClick={() => remove(index)}>Remove</button>}*/}
+            {/*                    </div>*/}
+            {/*                )*/}
+            {/*            })*/}
+
+            {/*        }*/}
+            {/*        <button type="button" onClick={() => append({number:""})}>Add Address</button>*/}
+            {/*    </div>*/}
+
+            {/*    <button type="submit">Submit</button>*/}
+            {/*</form>*/}
+            {/*<DevTool control={control}/>*/}
             <form onSubmit={handleSubmit(handleOnSubmit)} noValidate>
-                <label htmlFor="username">Name: </label>
-                <input type="text" id="name" {...register("name")}/>
-                {/*{errors.name?.message && <p className="error">{errors.name?.message}</p>}*/}
-                {errorValue?.name && <p className="error">{errorValue.name}</p>}
-                <br/>
-
-                <label htmlFor="department">Department: </label>
-                <input type="text" id="department" {...register("department")}/>
-                {errorValue?.department && <p className="error">{errorValue.department}</p>}
-                <br/>
-                <label htmlFor="companyName">Company Name: </label>
-                <input type="text" id="companyName" {...register("companyName")}/>
-                {errorValue?.companyName && <p className="error">{errorValue.companyName}</p>}
-                <br/>
-
-                <label htmlFor="addressIds">Address: </label>
-                <select id="addressIds" {...register("addressIds")} onChange={(e) => setAddressId(e.target.value)}>
-                    <option value="">::Select One::</option>
-                    {
-                        addressList.map((address) => {
-                            return(
-                                <option key={address.id} value={address.id}>{address.city}</option>
-                                )
-                        })
-                    }
-                </select>
-                <br/>
-
-                <input type="submit"/>
+                <h2 style={{textAlign: 'center'}}>Add An Employee</h2>
+                <Stack spacing={2} width={500} direction="column" justifyContent="center" ml={58}>
+                    <TextField
+                        error={errorValue?.name}
+                        type="text"
+                        label="Name"
+                        helperText={errorValue?.name}
+                        {...register('name')}
+                    />
+                    <TextField
+                        error={errorValue?.department}
+                        type="text"
+                        label="Department"
+                        helperText={errorValue?.department}
+                        {...register('department')}
+                    />
+                    <TextField
+                        error={errorValue?.companyName}
+                        type="text"
+                        label="Company Name"
+                        helperText={errorValue?.companyName}
+                        {...register('companyName')}
+                    />
+                    <TextField
+                        id="outlined-select-currency"
+                        select
+                        label="Select"
+                        {...register('addressIds')}
+                    >
+                        {addressList.map((address) => (
+                            <MenuItem key={address.id} value={address.id}>
+                                {address.city}
+                            </MenuItem>
+                        ))}
+                    </TextField>
+                    <Button type="submit" variant="contained" color="primary">Submit</Button>
+                </Stack>
             </form>
-            <DevTool control={control}/>
-            {/*<div>*/}
-            {/*    <p>{errorValue?.name}</p>*/}
-            {/*    <p>{errorValue?.department}</p>*/}
-            {/*    <p>{errorValue?.companyName}</p>*/}
-            {/*</div>*/}
-        </div>
+
+        </>
     )
 }
 export default YoutubeForm;
--- src/pages/EmployeeView.js
+++ src/pages/EmployeeView.js
@@ -1,5 +1,9 @@
 import {useEffect, useState} from "react";
 import axios from "axios";
+import { styled } from '@mui/material/styles';
+import {tableCellClasses} from '@mui/material/TableCell';
+import {Button, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow} from "@mui/material";
+import * as rows from "react-bootstrap/ElementChildren";
 
 const EmployeeView = () => {
 
@@ -9,6 +13,26 @@ const EmployeeView = () => {
     const [errorValue,setErrorValue] = useState(null)
     const [page,setPage] = useState(0)
     const [size,setSize] = useState(3)
+
+    const StyledTableCell = styled(TableCell)(({ theme }) => ({
+        [`&.${tableCellClasses.head}`]: {
+            backgroundColor: theme.palette.common.black,
+            color: theme.palette.common.white,
+        },
+        [`&.${tableCellClasses.body}`]: {
+            fontSize: 14,
+        },
+    }));
+
+    const StyledTableRow = styled(TableRow)(({ theme }) => ({
+        '&:nth-of-type(odd)': {
+            backgroundColor: theme.palette.action.hover,
+        },
+        // hide last border
+        '&:last-child td, &:last-child th': {
+            border: 0,
+        },
+    }));
 
     useEffect(() => {
         axios.get(`${baseEmpGetUrl}?page=${page}&size=${size}`).then(response => {
@@ -34,7 +58,7 @@ const EmployeeView = () => {
     return (
         <>
             <h3 style={{textAlign: "center"}}>Employee List</h3>
-            <div className="dropdown-pagination" style={{display: 'flex',gap: '2rem',justifyContent: 'flex-end'}}>
+            <div className="dropdown-pagination" style={{display: 'flex',gap: '1rem',justifyContent: 'flex-end'}}>
                 <select onChange={(e) => setPage(parseInt(e.target.value))}>
                     <option value="">::select page::</option>
                     <option value="0">1</option>
@@ -49,45 +73,81 @@ const EmployeeView = () => {
                     <option value="10">10</option>
                 </select>
             </div>
-            <div>
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Id</th>
-                            <th>Name</th>
-                            <th>Department</th>
-                            <th>Company</th>
-                            <th>Addresses</th>
-                            <th>Actions</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                    {
-                        employeeList.map((emp) => {
-                            return(
-                                <tr key={emp.id}>
-                                    <td>{emp.id}</td>
-                                    <td>{emp.name}</td>
-                                    <td>{emp.department}</td>
-                                    <td>{emp.companyName}</td>
-                                    <td>{
-                                        emp.addressList.map((address) => {
-                                            return `${address.city} `
-                                        })
-                                    }</td>
-                                    <td>
-                                        <button onClick={() => handleDelEmp(emp.id)}>Delete</button>
-                                    </td>
-                                </tr>
-                            )
-                        })
-                    }
-                    </tbody>
-                </table>
-            </div>
-            <div>
-                {errorValue?.error}
-            </div>
+            {/*<div>*/}
+            {/*    <table>*/}
+            {/*        <thead>*/}
+            {/*            <tr>*/}
+            {/*                <th>Id</th>*/}
+            {/*                <th>Name</th>*/}
+            {/*                <th>Department</th>*/}
+            {/*                <th>Company</th>*/}
+            {/*                <th>Addresses</th>*/}
+            {/*                <th>Actions</th>*/}
+            {/*            </tr>*/}
+            {/*        </thead>*/}
+            {/*        <tbody>*/}
+            {/*        {*/}
+            {/*            employeeList.map((emp) => {*/}
+            {/*                return(*/}
+            {/*                    <tr key={emp.id}>*/}
+            {/*                        <td>{emp.id}</td>*/}
+            {/*                        <td>{emp.name}</td>*/}
+            {/*                        <td>{emp.department}</td>*/}
+            {/*                        <td>{emp.companyName}</td>*/}
+            {/*                        <td>{*/}
+            {/*                            emp.addressList.map((address) => {*/}
+            {/*                                return `${address.city} `*/}
+            {/*                            })*/}
+            {/*                        }</td>*/}
+            {/*                        <td>*/}
+            {/*                            <button onClick={() => handleDelEmp(emp.id)}>Delete</button>*/}
+            {/*                        </td>*/}
+            {/*                    </tr>*/}
+            {/*                )*/}
+            {/*            })*/}
+            {/*        }*/}
+            {/*        </tbody>*/}
+            {/*    </table>*/}
+            {/*</div>*/}
+            <TableContainer component={Paper}>
+                <Table aria-label="simple table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Id</TableCell>
+                            <TableCell align="right">Name</TableCell>
+                            <TableCell align="right">Department</TableCell>
+                            <TableCell align="right">Company Name</TableCell>
+                            <TableCell align="right">Addresses</TableCell>
+                            <TableCell align="right">Action</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {employeeList.map((employee) => (
+                            <StyledTableRow
+                                key={employee.id}
+                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                            >
+                                <StyledTableCell component="th" scope="row">
+                                    {employee.id}
+                                </StyledTableCell>
+                                <StyledTableCell align="right">{employee.name}</StyledTableCell>
+                                <StyledTableCell align="right">{employee.department}</StyledTableCell>
+                                <StyledTableCell align="right">{employee.companyName}</StyledTableCell>
+                                <StyledTableCell align="right">{
+                                    `${employee.addressList.map(address => {
+                                        return `${address.city} `
+                                    })}`
+                                }</StyledTableCell>
+                                <StyledTableCell align="right">
+                                    <Button variant="contained" color="error" onClick={() => handleDelEmp(employee.id)}>
+                                        Delete
+                                    </Button>
+                                </StyledTableCell>
+                            </StyledTableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
         </>
     )
 }
